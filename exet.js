fff7ac98b617734a2fd21d242104165621fa7358
Ian Hunter <Ian.Hunter@amd.com>
2026-08-11 15:45:30 +0100

    various fixes

--- exet.js
+++ exet.js
@@ -154,6 +154,9 @@ function Exet() {
   this.throttledCharadeTimer = null;
   this.viabilityUpdateTimer = null;
   this.throttledLightRegexpTimer = null;
+  /** headword (lower) -> expansions[], from lists/abbreviations.json */
+  this.abbrevMap = null;
+  this.abbrevLoadPromise = null;
   this.inputLagMS = 400;
   this.longInputLagMS = 2000;
   this.sweepMS = 500;
@@ -199,7 +202,8 @@ function Exet() {
      using
      <i>Edit &gt; Add/Edit special sections: &gt; Preamble</i>.`,
     `Wordplay tabs such as "Charades" and "Anagrams" show candidate wordplays
-     for the currently selected entry. However, you can edit the fodder text
+     for the currently selected entry. In Charades, words with dotted underlines
+     show cryptic abbreviation expansions on hover. You can edit the fodder text
      directly in the tab to experiment with alternatives.`,
     `Saving the crossword as HTML (Exolve format) with solutions included
      can be done using the keyboard shortcut Ctrl-s (Cmd-s on Mac). Exet
@@ -2726,6 +2730,162 @@ Exet.prototype.getAllSplits = function(fodder, k) {
   return splits;
 }
 
+/**
+ * Load offline cryptic abbreviations for charade hover hints.
+ */
+Exet.prototype.buildAbbrevByLetter = function() {
+  this.abbrevByLetter = new Map();
+  if (!this.abbrevMap) {
+    return;
+  }
+  for (const [hwLower, exps] of this.abbrevMap) {
+    for (const exp of exps) {
+      if (exp.length == 1 && /[A-Za-z]/.test(exp)) {
+        const letter = exp.toLowerCase();
+        let headwords = this.abbrevByLetter.get(letter);
+        if (!headwords) {
+          headwords = [];
+          this.abbrevByLetter.set(letter, headwords);
+        }
+        headwords.push(hwLower);
+      }
+    }
+  }
+}
+
+Exet.prototype.initAbbrevMap = function() {
+  if (this.abbrevMap) {
+    return;
+  }
+  if (typeof exetAbbrevLookup !== 'object' || !exetAbbrevLookup) {
+    return;
+  }
+  this.abbrevMap = new Map();
+  for (const hw in exetAbbrevLookup) {
+    if (Object.prototype.hasOwnProperty.call(exetAbbrevLookup, hw)) {
+      this.abbrevMap.set(hw.toLowerCase(), exetAbbrevLookup[hw]);
+    }
+  }
+  this.buildAbbrevByLetter();
+}
+
+Exet.prototype.loadAbbreviations = function() {
+  this.initAbbrevMap();
+  if (this.abbrevMap) {
+    return Promise.resolve();
+  }
+  if (this.abbrevLoadPromise) {
+    return this.abbrevLoadPromise;
+  }
+  this.abbrevLoadPromise = fetch('lists/abbreviations.json')
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error('abbreviations.json HTTP ' + resp.status);
+      }
+      return resp.json();
+    })
+    .then(data => {
+      const map = new Map();
+      for (const entry of (data.entries || [])) {
+        map.set(entry.headword.toLowerCase(), entry.expansions);
+      }
+      this.abbrevMap = map;
+      this.buildAbbrevByLetter();
+      if (this.charadeFodder && this.charades && this.charades.innerHTML) {
+        const paramElt = document.getElementById('xet-charades-param');
+        if (paramElt) {
+          this.updateCharades(paramElt.value);
+        }
+      }
+    })
+    .catch(err => {
+      console.warn('Exet: could not load abbreviations list', err);
+      this.abbrevMap = new Map();
+    });
+  return this.abbrevLoadPromise;
+}
+
+Exet.prototype.getAbbrevTooltip = function(word) {
+  if (!this.abbrevMap || !word) {
+    return '';
+  }
+  const exps = this.abbrevMap.get(word.toLowerCase());
+  if (!exps || exps.length == 0) {
+    return '';
+  }
+  return exps.join(', ');
+}
+
+Exet.prototype.getAbbrevLetterTooltip = function(letter) {
+  if (!this.abbrevByLetter || !letter) {
+    return '';
+  }
+  const headwords = this.abbrevByLetter.get(letter.toLowerCase());
+  if (!headwords || headwords.length == 0) {
+    return '';
+  }
+  const max = 30;
+  let tip = headwords.slice(0, max).join(', ');
+  if (headwords.length > max) {
+    tip += ', …';
+  }
+  return tip;
+}
+
+Exet.prototype.escapeAttr = function(s) {
+  return String(s).replace(/&/g, '&amp;').replace(/"/g, '&quot;')
+                  .replace(/</g, '&lt;');
+}
+
+Exet.prototype.wrapAbbrevDisplayHtml = function(html, rawWord) {
+  const tip = this.getAbbrevTooltip(rawWord);
+  if (!tip) {
+    return html;
+  }
+  return '<span class="xet-charade-word xet-abbrev-hint" title="' +
+         this.escapeAttr(tip) + '">' + html + '</span>';
+}
+
+Exet.prototype.annotateAbbrevInHtml = function(html) {
+  if (!this.abbrevMap) {
+    return html;
+  }
+  const exet = this;
+  return html.replace(
+      /<span class="(xet-green|xet-darkgreen)">([a-zA-Z]+)<\/span>/g,
+      function(match, cls, letters) {
+        let tip = '';
+        if (letters.length == 1) {
+          tip = exet.getAbbrevLetterTooltip(letters) ||
+                exet.getAbbrevTooltip(letters);
+        } else {
+          tip = exet.getAbbrevTooltip(letters);
+        }
+        if (!tip) {
+          return match;
+        }
+        return '<span class="xet-charade-word xet-abbrev-hint" title="' +
+               exet.escapeAttr(tip) + '"><span class="' + cls + '">' +
+               letters + '</span></span>';
+      });
+}
+
+Exet.prototype.formatCharadeChoices = function(part, anagramWords) {
+  const display = exetLexicon.displayAnagrams(part, anagramWords);
+  if (display.length == 0) {
+    return '';
+  }
+  const wrapped = display.map((disp) => {
+    return this.annotateAbbrevInHtml(disp);
+  });
+  let text = wrapped.join(', ');
+  if (display.length > 1) {
+    text = '<span class="xet-blue">[</span>' + text +
+           '<span class="xet-blue">]</span>';
+  }
+  return text;
+}
+
 Exet.prototype.pushCharadeCandidate = function(elements) {
   if (!elements || elements.length == 0) {
     return
@@ -2801,6 +2961,7 @@ Exet.prototype.maybeTrimLongFodder = function(fodderArr, name) {
 }
 
 Exet.prototype.updateCharades = function(fodder) {
+  this.loadAbbreviations();
   if (this.throttledCharadeTimer) {
     clearTimeout(this.throttledCharadeTimer);
   }
@@ -2831,7 +2992,7 @@ Exet.prototype.addDeletionCharades = function() {
       diffAnagsStr = '<span class="xet-blue">[</span>' + diffAnagsStr +
                      '<span class="xet-blue">]</span>';
     }
-    minuses.push(diffAnagsStr);
+    minuses.push(this.annotateAbbrevInHtml(diffAnagsStr));
     /**
      * High score to put all deletions on top of other charades, for
      * convenience. Shorter deletions are shown first.
@@ -2840,7 +3001,8 @@ Exet.prototype.addDeletionCharades = function() {
   }
   for (let i = 0; i < words.length; i++) {
     const charadeStruct = {
-      charade: '<span class="xet-blue">*</span>(' + words[i] +
+      charade: '<span class="xet-blue">*</span>(' +
+               this.wrapAbbrevDisplayHtml(this.escapeHtml(words[i]), words[i]) +
                ' <span class="xet-blue">minus</span> ' +
                minuses[i] + ')',
       score: scores[i],
@@ -2867,16 +3029,11 @@ Exet.prototype.updateCharadesPartial = function(work=100, sleep=50) {
       for (let part of split) {
         let possible = '';
         let score = 0;
-        const choices = exetLexicon.displayAnagrams(
-            part, exetLexicon.getAnagrams(part, 6, false));
-        if (choices.length > 0) {
-          possible = choices.join(', ');
+        const anagramIndices = exetLexicon.getAnagrams(part, 6, false);
+        if (anagramIndices.length > 0) {
+          possible = this.formatCharadeChoices(part, anagramIndices);
           const partLetters = exetLexicon.lettersOf(part);
           score = partLetters.length;
-          if (choices.length > 1) {
-            possible = '<span class="xet-blue">[</span>' + possible +
-                       '<span class="xet-blue">]</span>';
-          }
         }
         viable.push({possible: possible, score: score});
       }
@@ -2906,15 +3063,10 @@ Exet.prototype.updateCharadesPartial = function(work=100, sleep=50) {
             continue;
           }
           const container = split[c1] + (split[c2]);
-          const choices = exetLexicon.displayAnagrams(
-              container, exetLexicon.getAnagrams(container, 5, true));
-          if (choices.length > 0) {
-            let possible = choices.join(', ');
+          const containerIndices = exetLexicon.getAnagrams(container, 5, true);
+          if (containerIndices.length > 0) {
+            const possible = this.formatCharadeChoices(container, containerIndices);
             const containerParts = exetLexicon.lettersOf(container);
-            if (choices.length > 1) {
-              possible = '<span class="xet-blue">[</span>' + possible +
-                         '<span class="xet-blue">]</span>';
-            }
             let vcopy = viable.slice();
             vcopy[c1] = {};
             vcopy[c1].possible = possible;
@@ -3479,7 +3631,7 @@ Exet.prototype.populateFrame = function() {
   for (const id in this.tabs) {
     const tab = this.tabs[id];
     tab.button = document.getElementById(`xet-${id}`);
-    tab.button.title = tab.hover;
+    tab.button.title = tab.display + (tab.hover ? ': ' + tab.hover : '');
     const handler = this.handleTabClick.bind(this, id);
     tab.button.addEventListener('click', handler);
     tab.frame = document.getElementById(`xet-${id}-frame`);
@@ -4352,23 +4504,35 @@ Exet.prototype.resizeRHS = function() {
     .xet-frame {
       width: ${frameW}px;
     }
+    .xet-tab {
+      width: ${frameW}px;
+    }
     .xet-tab-content {
       height: ${500 + extraH}px;
       width: ${frameW}px;
+    }
+  `;
+  const tabCount = Math.max(1, Object.keys(this.tabs).length);
+  const tabSlotW = Math.floor((frameW - 8) / tabCount);
+  let maxLabelLen = 0;
+  for (const id in this.tabs) {
+    const len = (this.tabs[id].display || '').length;
+    if (len > maxLabelLen) {
+      maxLabelLen = len;
+    }
+  }
+  /** ~0.52em per character keeps longest label on one line. */
+  const tabFont = Math.min(14, Math.max(8,
+      Math.floor((tabSlotW - 6) / (maxLabelLen * 0.52))));
+  style += `
+    .xet-tab button {
+      font-size: ${tabFont}px;
     }
   `;
   if (frameW < 860) {
     style += `
       .xet-analysis {
         right: 0;
-      }
-      .xet-tab button {
-        font-size: 11px;
-        width: 70px;
-      }
-      .xet-tab button:hover {
-        font-size: 12px;
-        width: 80px;
       }
     `;
   }
@@ -7656,6 +7820,7 @@ Exet.prototype.periodicChecks = function() {
 }
 
 Exet.prototype.finishSetup = function() {
+  this.loadAbbreviations();
   this.versionText = '';
   this.periodicChecks();
   /** Check every 10 minutes */
